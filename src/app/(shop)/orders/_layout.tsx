3ccdb7ff76095bddb593871bbff847beaef74f63
snow6692 <ahmedha258258@gmail.com>
2025-02-16 20:03:41 +0200

    Real time status order

--- src/app/(shop)/orders/_layout.tsx
+++ src/app/(shop)/orders/_layout.tsx
@@ -1,15 +1,15 @@
-import { View, Text } from "react-native";
 import React from "react";
 import { Stack } from "expo-router";
+import { useOrderUpdateSubscription } from "../../../api/subscription";
 
 const OrdersLayout = () => {
+  useOrderUpdateSubscription();
   return (
     <Stack>
       <Stack.Screen
         name="index"
         options={{
           headerShown: false,
-          
         }}
       />
     </Stack>
